4d731e5ecceaaf1f63731c6571e12246fc83ca9f
Simon Cozens <simon@simon-cozens.org>
2019-02-07 10:54:34 +0000

    Initial Commit

---
+++ Brownie/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Brownie
+//
+//  Created by Simon Cozens on 07/02/2019.
+//  Copyright © 2019 Simon Cozens. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Brownie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Brownie
+//
+//  Created by Simon Cozens on 07/02/2019.
+//  Copyright © 2019 Simon Cozens. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
